976460e6307a96a8b84d29da5d0e6b1bf4799df5
Max Petersson <max.petersson@gmail.com>
2024-03-23 17:50:03 +0100

    refactor gameboard into vite project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import './style.css'
 import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+   
+//     <h1>T3</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//   </div>
+// `
+
+document.querySelector<HTMLDivElement>('#gameBoard')!.innerHTML = `
   <div>
    
     <h1>T3</h1>
