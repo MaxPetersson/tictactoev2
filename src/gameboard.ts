4e9f43fbbaba7b715a25c67f1b4b09da7f5d9273
Max Petersson <max.petersson@gmail.com>
2024-04-10 14:47:26 +0200

    Add Prettier as format standard

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -3,178 +3,188 @@ let numberOfSquares: number = nrsInRow * nrsInRow;
 let gameBoard: GameBoard;
 
 class GamePiece {
-    pieceDiv: HTMLDivElement;
+  pieceDiv: HTMLDivElement;
 
-    constructor() {
-        this.pieceDiv = document.createElement('div') as HTMLDivElement;
-        this.pieceDiv.id = 'gamePiece';
+  constructor() {
+    this.pieceDiv = document.createElement("div") as HTMLDivElement;
+    this.pieceDiv.id = "gamePiece";
+  }
+
+  fetchImage(imageName: string): HTMLImageElement {
+    let imgElement = document.createElement("img");
+    imgElement.setAttribute("src", "../images/" + imageName);
+    imgElement.setAttribute("height", "100");
+    imgElement.setAttribute("width", "100");
+
+    return imgElement;
+  }
+
+  setGamePiece(piece: string): void {
+    if (piece == "x") {
+      this.pieceDiv.appendChild(this.fetchImage("x.jpg"));
+    } else {
+      this.pieceDiv.appendChild(this.fetchImage("o.jpg"));
     }
-
-    fetchImage(imageName: string): HTMLImageElement {
-        let imgElement = document.createElement("img");
-        imgElement.setAttribute("src", "../images/" + imageName);
-        imgElement.setAttribute("height", "100");
-        imgElement.setAttribute("width", "100");
-
-        return imgElement;
-    }
-
-    setGamePiece(piece: string): void {
-        if (piece == 'x') {
-            this.pieceDiv.appendChild(this.fetchImage("x.jpg"));
-        }
-        else {
-            this.pieceDiv.appendChild(this.fetchImage("o.jpg"));
-        }
-    }
+  }
 }
 
 class GameSquare {
-    gameSquare: HTMLDivElement;
-    gamePiece!: GamePiece;
-    isX: boolean = false;
-    isO: boolean = false;
-    mouseOver: EventListener;
-    mouseLeave: EventListener;
-    click: EventListener;
+  gameSquare: HTMLDivElement;
+  gamePiece!: GamePiece;
+  isX: boolean = false;
+  isO: boolean = false;
+  mouseOver: EventListener;
+  mouseLeave: EventListener;
+  click: EventListener;
 
-    constructor() {
-        this.gameSquare = document.createElement('div') as HTMLDivElement;
-        this.gameSquare.id = 'gameSquare';
+  constructor() {
+    this.gameSquare = document.createElement("div") as HTMLDivElement;
+    this.gameSquare.id = "gameSquare";
 
-        this.mouseOver = () => { this.highlight() };
-        this.mouseLeave = () => { this.unhighlight() };
-        this.click = () => {
-            this.removeEventListeners();
-            this.unhighlight();
-            gameBoard.playTurn(this);
-        };
+    this.mouseOver = () => {
+      this.highlight();
+    };
+    this.mouseLeave = () => {
+      this.unhighlight();
+    };
+    this.click = () => {
+      this.removeEventListeners();
+      this.unhighlight();
+      gameBoard.playTurn(this);
+    };
 
-        this.addEventListeners();
+    this.addEventListeners();
+  }
+
+  highlight(): void {
+    this.gameSquare.style.backgroundColor = "lightSalmon";
+  }
+
+  highlightVictory(): void {
+    this.gameSquare.style.backgroundColor = "lightGreen";
+  }
+
+  unhighlight(): void {
+    this.gameSquare.style.backgroundColor = "white";
+  }
+
+  removeEventListeners(): void {
+    this.gameSquare.removeEventListener("mouseover", this.mouseOver);
+    this.gameSquare.removeEventListener("mouseleave", this.mouseLeave);
+    this.gameSquare.removeEventListener("click", this.click);
+  }
+
+  addEventListeners(): void {
+    this.gameSquare.addEventListener("mouseover", this.mouseOver);
+    this.gameSquare.addEventListener("mouseleave", this.mouseLeave);
+    this.gameSquare.addEventListener("click", this.click);
+  }
+
+  resetGameSquare(): void {
+    let gamePiece = this.gameSquare.firstChild;
+    if (gamePiece) {
+      this.gameSquare.removeChild(gamePiece);
+      this.addEventListeners();
     }
+  }
 
-    highlight(): void {
-        this.gameSquare.style.backgroundColor = 'lightSalmon';
-    }
+  setGamePiece(gamePiece_: GamePiece): void {
+    this.gamePiece = gamePiece_;
+  }
 
-    highlightVictory(): void {
-        this.gameSquare.style.backgroundColor = 'lightGreen';
+  renderGamePiece(): void {
+    if (this.gamePiece) {
+      this.gameSquare.appendChild(this.gamePiece.pieceDiv);
     }
-
-    unhighlight(): void {
-        this.gameSquare.style.backgroundColor = 'white';
-    }
-
-    removeEventListeners(): void {
-        this.gameSquare.removeEventListener("mouseover", this.mouseOver);
-        this.gameSquare.removeEventListener("mouseleave", this.mouseLeave);
-        this.gameSquare.removeEventListener("click", this.click);
-    }
-
-    addEventListeners(): void {
-        this.gameSquare.addEventListener("mouseover", this.mouseOver);
-        this.gameSquare.addEventListener("mouseleave", this.mouseLeave);
-        this.gameSquare.addEventListener("click", this.click);
-    }
-
-    resetGameSquare(): void {
-        let gamePiece = this.gameSquare.firstChild;
-        if (gamePiece) {
-            this.gameSquare.removeChild(gamePiece);
-            this.addEventListeners();
-        }
-    }
-
-    setGamePiece(gamePiece_: GamePiece): void {
-        this.gamePiece = gamePiece_;
-    }
-
-    renderGamePiece(): void {
-        if (this.gamePiece){
-            this.gameSquare.appendChild(this.gamePiece.pieceDiv);
-        }
-    }
+  }
 }
 
 class GameBoard {
-    gameBoard: HTMLDivElement;
-    boardSize: number;
-    gameSquares: Array<GameSquare>;
-    turn: string = "x";
+  gameBoard: HTMLDivElement;
+  boardSize: number;
+  gameSquares: Array<GameSquare>;
+  turn: string = "x";
 
-    constructor(gameBoard_: HTMLDivElement) {
-        this.gameBoard = gameBoard_;
-        this.boardSize = numberOfSquares;
-        this.gameSquares = new Array(numberOfSquares)
+  constructor(gameBoard_: HTMLDivElement) {
+    this.gameBoard = gameBoard_;
+    this.boardSize = numberOfSquares;
+    this.gameSquares = new Array(numberOfSquares);
+  }
+
+  init() {
+    for (let i = 0; i < this.boardSize; i++) {
+      this.gameSquares[i] = new GameSquare();
+      this.gameBoard.appendChild(this.gameSquares[i].gameSquare);
+    }
+  }
+
+  reset() {
+    this.gameSquares.forEach((gameSquare: GameSquare) =>
+      gameSquare.resetGameSquare()
+    );
+    this.turn = "x";
+  }
+
+  playTurn(gameSquare: GameSquare) {
+    let gamePiece: GamePiece = new GamePiece();
+
+    if (this.turn == "x") {
+      gamePiece.setGamePiece("x");
+      gameSquare.isX = true;
+    } else {
+      gamePiece.setGamePiece("o");
+      gameSquare.isO = true;
     }
 
-    init() {
-        for (let i = 0; i < this.boardSize; i++) {
-            this.gameSquares[i] = new GameSquare();
-            this.gameBoard.appendChild(this.gameSquares[i].gameSquare);
-        }
+    gameSquare.setGamePiece(gamePiece);
+    gameSquare.renderGamePiece();
+
+    if (this.calculateVictory()) {
+      this.gameSquares.forEach((element) => {
+        element.removeEventListeners();
+      });
     }
 
-    reset() {
-        this.gameSquares.forEach((gameSquare: GameSquare) => gameSquare.resetGameSquare());
-        this.turn = "x";
+    // Next player's turn.
+    this.turn = this.turn == "x" ? "o" : "x";
+  }
+
+  checkRow(square1: number, square2: number, square3: number): boolean {
+    let isVictory: boolean = false;
+    const yVictory: boolean =
+      this.gameSquares[square1].isO &&
+      this.gameSquares[square2].isO &&
+      this.gameSquares[square3].isO;
+    const oVictory: boolean =
+      this.gameSquares[square1].isX &&
+      this.gameSquares[square2].isX &&
+      this.gameSquares[square3].isX;
+    isVictory = yVictory || oVictory;
+    if (isVictory) {
+      this.gameSquares[square1].highlightVictory();
+      this.gameSquares[square2].highlightVictory();
+      this.gameSquares[square3].highlightVictory();
     }
+    return isVictory;
+  }
 
-    playTurn(gameSquare: GameSquare) {
-        let gamePiece: GamePiece = new GamePiece();
+  calculateVictory(): boolean {
+    let isVictory: boolean = false;
 
-        if (this.turn == "x") {
-            gamePiece.setGamePiece('x')
-            gameSquare.isX = true;
-        }
-        else {
-            gamePiece.setGamePiece('o')
-            gameSquare.isO = true;
-        }
-
-        gameSquare.setGamePiece(gamePiece);
-        gameSquare.renderGamePiece();
-
-        if (this.calculateVictory()) {
-            this.gameSquares.forEach(element => {
-                element.removeEventListeners();
-            });
-        }
-
-        // Next player's turn.
-        this.turn = this.turn == "x" ? "o" : "x";
-    }
-
-    checkRow(square1: number, square2: number, square3: number): boolean {
-        let isVictory: boolean = false;
-        const yVictory: boolean = this.gameSquares[square1].isO && this.gameSquares[square2].isO && this.gameSquares[square3].isO;
-        const oVictory: boolean = this.gameSquares[square1].isX && this.gameSquares[square2].isX && this.gameSquares[square3].isX;
-        isVictory = yVictory || oVictory;
-        if (isVictory){
-            this.gameSquares[square1].highlightVictory();
-            this.gameSquares[square2].highlightVictory();
-            this.gameSquares[square3].highlightVictory();
-        }
-        return isVictory;
-    }
-
-    calculateVictory(): boolean {
-        let isVictory: boolean = false;
-
-        isVictory = isVictory || this.checkRow(0,1,2);
-        isVictory = isVictory || this.checkRow(3,4,5);
-        isVictory = isVictory || this.checkRow(6,7,8);
-        isVictory = isVictory || this.checkRow(0,3,6);
-        isVictory = isVictory || this.checkRow(1,4,7);
-        isVictory = isVictory || this.checkRow(2,5,8);
-        isVictory = isVictory || this.checkRow(0,4,8);
-        isVictory = isVictory || this.checkRow(2,4,6);
-        return isVictory;
-    }
+    isVictory = isVictory || this.checkRow(0, 1, 2);
+    isVictory = isVictory || this.checkRow(3, 4, 5);
+    isVictory = isVictory || this.checkRow(6, 7, 8);
+    isVictory = isVictory || this.checkRow(0, 3, 6);
+    isVictory = isVictory || this.checkRow(1, 4, 7);
+    isVictory = isVictory || this.checkRow(2, 5, 8);
+    isVictory = isVictory || this.checkRow(0, 4, 8);
+    isVictory = isVictory || this.checkRow(2, 4, 6);
+    return isVictory;
+  }
 }
 
 export function gameboardInit(element: HTMLDivElement) {
-    gameBoard = new GameBoard(element);
+  gameBoard = new GameBoard(element);
 
-    gameBoard.init();
+  gameBoard.init();
 }
