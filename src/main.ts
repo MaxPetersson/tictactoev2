8bf4846492416812ced37ef7cf60388245313e96
Max Petersson <max.petersson@gmail.com>
2024-04-10 14:49:56 +0200

    cleanup main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,4 @@
-import './style.css'
-//import { setupCounter } from './counter.ts'
-import { gameboardInit } from './gameboard.ts';
+import "./style.css";
+import { gameboardInit } from "./gameboard.ts";
 
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-   
-//     <h1>T3</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//   </div>
-// `
-
-// document.querySelector<HTMLDivElement>('#gameBoard')!.innerHTML = `
-//   <div id="gameboardDiv">
-//   </div>
-// `
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
-gameboardInit(document.querySelector<HTMLDivElement>('#gameBoard')!)
+gameboardInit(document.querySelector<HTMLDivElement>("#gameBoard")!);
