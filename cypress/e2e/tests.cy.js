0b5ed746c2b71e5ab91eab363fc46c53c706e7b4
Max Petersson <max.petersson@gmail.com>
2024-04-17 08:26:45 +0200

    cleanup formatting and fix mouseover TC

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -3,28 +3,26 @@
 describe("tictactoe", () => {
   beforeEach(() => {
     // Run before each test case, basically - visit the page.
-    cy.visit("http://localhost:5173/");
+    cy.visit("http://localhost:5174/");
   });
 
   it("Checking so a gamesquare exists, yay.", () => {
     // Use cy.get() to select the div element by its class
     cy.get(".gameSquare").should("have.length", 9);
   });
+
   it("Check that gameBoard exists.", () => {
-    cy.get('.gameBoard').should('exist');
+    cy.get(".gameBoard").should("exist");
   });
-it('check mouseover', () => {
-  // Assuming you already have the page loaded and don't need to visit it again
 
-  // Trigger mouseover event on the .gameSquare element
-  cy.get('.gameSquare').trigger('mouseover');
+  it("check mouseover", () => {
+    // Trigger mouseover event on the first .gameSquare element
+    cy.get(".gameSquare").eq(0).trigger("mouseover");
 
-  // Check if the color of the .gameSquare changes to 'lightSalmon'
-  cy.get('.gameSquare')
-    .should('have.css', 'background-color')
-    .and('equal', 'rgb(255, 160, 122)'); // 'lightSalmon' color in rgb format
-});
-
-
-
+    // Check if the color of the first .gameSquare changes to 'lightSalmon'
+    cy.get(".gameSquare")
+      .eq(0)
+      .should("have.css", "background-color")
+      .and("equal", "rgb(255, 160, 122)"); // 'lightSalmon' color in rgb format
+  });
 });
